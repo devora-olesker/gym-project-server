c2820bb6ca78c22ba3f201a5199190eb0d860a5f
Devora <devoraolesker@gmail.com>
2022-11-02 17:29:58 +0200

    login user funcion- dosent work

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,7 +1,14 @@
 const express = require("express")
 const router = express.Router()
 const { PrismaClient } = require('@prisma/client')
+const bodyParser = require("body-parser")
 const prisma = new PrismaClient()
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+ 
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 
 //gets an id and returns the matching user
@@ -12,18 +19,18 @@ router.get('/getUserById/:id', async (req, res) => {
                 id: req.params.id
             }
         })
-        if(user){
+        if (user) {
             res.status = 200
             res.send(user)
         }
-        else{
+        else {
             res.status(400)
             res.send('User does not exist')
         }
-      
+
     }
     catch (error) {
-        res.status(404) 
+        res.status(404)
         console.log('Can not get data from db...')
         res.end()
     }
@@ -34,35 +41,72 @@ router.get('/getUserById/:id', async (req, res) => {
 router.get('/logIn/:id/:password', async (req, res) => {
     try {
         //check if user exiests
-        const user= await prisma.users.findUnique({
-            where:{
-                id:req.params.id
+        const user = await prisma.users.findUnique({
+            where: {
+                id: req.params.id
             }
         })
-        if(!user){
+        if (!user) {
             res.status(400)
             res.send('User does not exist')
         }
         //if user does exist, check if password is correct
-        else{
-            if(user.password!= req.params.password){
+        else {
+            if (user.password != req.params.password) {
                 res.status(400)
                 res.send('Password is incorrect')
             }
             //if everything is fine, return the user
-            else{
+            else {
                 res.status(200)
                 res.send(user)
             }
         }
     }
     catch (error) {
-        res.status = 404
+        res.status(404)
         console.log('Can not get data from db...')
         res.end()
     }
 })
 
+//gets a user object in the body and checks if the user exists. 
+//if it allredy exiests, it will return a message. if not it will add the new user to the db
+router.post('/addNewUser/:id', async (req, res) => {
+    try {
+        // const { id, password, firstName, lastName, email, phone, dateOfBirth } = await req.body
+        // const newUser = req.body
+        // console.log(newUser.id);
+        //check if user id allready exiests
+        const user = await prisma.users.findUnique({ where: { id: req.params.id } })
+        if (user) {
+            res.status(400)
+            res.send('User allready exiests')
+        }
+        //if the user dosent exiest, it will be aded to the db
+        else {
+            console.log('jjj');
+            const user = await prisma.users.create({
+                 data: {
+                    id:req.body.id,
+                    password:req.body.password,
+                    firstName: req.body.firstName,
+                    lastName: req.body.lastName,
+                    email:req.body.email,
+                    phone:req.body.phone,
+                    dateOfBirth:req.body.dateOfBirth,
+                 }
+                })
+            res.status(200)
+            res.send(user)
+        }
 
+    } catch (error) {
+        res.status(404)
+        res.statusMessage = 'Can not get data from db...'
+        console.log('Can not get data from db...'+error)
+        res.end()
+    }
+})
 
 module.exports = router

7c35a1b632cda370871bdd0b31788aa63c769939
Devora <devoraolesker@gmail.com>
2023-01-26 21:29:13 +0200

    deal with all the problems, merge both of our stuffso we can create btraches

--- controllers/MachinesController.js
+++ controllers/MachinesController.js
@@ -27,9 +27,25 @@ router.get("/getAllMachines", async (req, res) => {
         console.log('Can not get data from db...')
         res.end()
     }
-
 })
-
+//gets a id and an updated machine objet, and updates the machine with that id
+//returns the updated machine
+router.patch('/updateMachine/:id', async (req, res) => {
+    try {
+        const updatedMachine = await prisma.exersizeMachines.update({
+            where: {
+                id: parseInt(req.params.id)
+            },
+            data: req.body
+        })
+        res.status(200)
+        res.send(updatedMachine)
+    } catch (error) {
+        res.status(400)
+        console.log('Can not get data from db... ' + error)
+        res.send(error)
+    }
+})
 
 //gets an id and returns the matching machiens
 router.get('/getMachineById/:id', async (req, res) => {
@@ -46,7 +62,6 @@ router.get('/getMachineById/:id', async (req, res) => {
                         workoutLength: true,
                         Instructions: true,
                     }
-
                 },
                 machinePurposes: {
                     select: { goal: true }
@@ -61,7 +76,6 @@ router.get('/getMachineById/:id', async (req, res) => {
             res.status(400)
             res.send('mechien does not exist')
         }
-
     }
     catch (error) {
         res.status(400)
@@ -72,16 +86,14 @@ router.get('/getMachineById/:id', async (req, res) => {
 //gets an id and deletes the Machine with that id. returns the new list of Machine
 router.delete('/deleteMachine/:id', async (req, res) => {
     try {
-        await prisma.machine.delete({
+        await prisma.exersizeMachines.delete({
             where: {
-                id: req.params.id
+                id: parseInt(req.params.id)
             }
         })
-        const machines = await prisma.users.findMany({})
+        const machines = await prisma.exersizeMachines.findMany({})
         res.status(200)
         res.send(machines)
-
-
     }
     catch (error) {
         res.status(400)
@@ -89,6 +101,50 @@ router.delete('/deleteMachine/:id', async (req, res) => {
         res.send(error)
     }
 })
-
+//Gets a workout goal id, and returns all machines with that goal	
+router.get('/getMachinesByWorkoutGoal/:goalId', async (req, res) => {
+    try {
+        const machiens = await prisma.machinePurposes.findMany({
+            where: {
+                goalId: parseInt(req.params.goalId)
+            },
+            select: {
+                machines: {
+                    include: {
+                        difficultyLevels: {
+                            select: {
+                                numLevel: true,
+                                workoutLength: true,
+                                Instructions: true,
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        res.status = 200
+        res.send(machiens)
+    }
+    catch (error) {
+        res.status = 404
+        console.log('Can not get data from db...')
+        res.end()
+    }
+})
+//gets a machine object in the body and adds the new machine to the db	
+router.post('/addNewMachine', async (req, res) => {
+    try {
+        const machineToAdd = await req.body
+        const newMachine = await prisma.exersizeMachines.create({
+            data: machineToAdd
+        })
+        res.status(200)
+        res.send(newMachine)
+    } catch (error) {
+        res.status(400)
+        console.log('Can not get data from db...' + error)
+        res.send('Can not get data from db...' + error)
+    }
+})
 
 module.exports = router
